c6f815fad359e69f7d3e73a6a9ee55e65c9398f0
spatchcock <andrew.berkeley.is@googlemail.com>
2011-08-03 14:37:24 +0100

    move Term#convert_units to DA module

--- lib/amee/data_abstraction/term_reporting_support.rb
+++ lib/amee/data_abstraction/term_reporting_support.rb
@@ -30,45 +30,6 @@ module AMEE
     #
     module TermReportingSupport
 
-      # Return a new instance of <i>Term</i>, based on <tt>self</tt> but with
-      # a change of units, according to the <tt>options</tt> hash provided, and
-      # the value attribute updated to reflect the new units.
-      #
-      # To specify a new unit, pass the required unit via the <tt>:unit</tt> key.
-      # To specify a new per_unit, pass the required per unit via the
-      # <tt>:per_unit</tt> key. E.g.,
-      #
-      #   my_term.convert_unit(:unit => :kg)
-      #
-      #   my_term.convert_unit(:unit => :kg, :per_unit => :h)
-      #
-      #   my_term.convert_unit(:unit => 'kilogram')
-      #
-      #   my_term.convert_unit(:per_unit => Quantify::Unit.h)
-      #
-      #   my_term.convert_unit(:unit => <Quantify::Unit::SI ... >)
-      #
-      # If <tt>self</tt> does not hold a numeric value or either a unit or per
-      # unit attribute, <tt.self</tt> is returned.
-      #
-      def convert_unit(options={})
-        return self unless has_numeric_value? and (unit or per_unit)
-        new = clone
-        if options[:unit] and unit
-          new_unit = Unit.for(options[:unit])
-          Term.validate_dimensional_equivalence?(unit,new_unit)
-          new.value Quantity.new(new.value,new.unit).to(new_unit).value
-          new.unit options[:unit]
-        end
-        if options[:per_unit] and per_unit
-          new_per_unit = Unit.for(options[:per_unit])
-          Term.validate_dimensional_equivalence?(per_unit,new_per_unit)
-          new.value Quantity.new(new.value,(1/new.per_unit)).to(Unit.for(new_per_unit)).value
-          new.per_unit options[:per_unit]
-        end
-        return new
-      end
-
       # Returns an instance of <i>Result</i> based upon the attributes of
       # <tt>self</tt>.
       #
